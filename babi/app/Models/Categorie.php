9dd3e6d2a22465d6d9a54cd628cedfd7a6c55809
Sory <sidibesory204@gmail.com>
2026-06-13 11:04:33 +0000

    feat: add categorie model, migration, controller, requests and api routes

---
+++ babi/app/Models/Categorie.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    protected $primaryKey = 'id_categorie';
+
+    protected $fillable = [
+        'nom_categorie',
+        'description',
+    ];
+
+    public function prestataires()
+    {
+        return $this->hasMany(Prestataire::class, 'id_categorie');
+    }
+
+    public function services()
+    {
+        return $this->hasMany(Service::class, 'id_categorie');
+    }
+}
--- babi/routes/api.php
+++ babi/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-
 use App\Http\Controllers\Api\PrestaireController;
+use App\Http\Controllers\Api\CategorieController;
 
 Route::apiResource('prestataires', PrestaireController::class);
+Route::apiResource('categories', CategorieController::class);
